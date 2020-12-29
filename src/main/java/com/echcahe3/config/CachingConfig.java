d6ac940a66bead64a2ab9bd7a6adf2bd38d87189
ms.seol <ms.seol@npixel.co.kr>
2020-12-29 17:07:46 +0900

    update

--- src/main/java/com/echcahe3/config/CachingConfig.java
+++ src/main/java/com/echcahe3/config/CachingConfig.java
@@ -1,6 +1,7 @@
 package com.echcahe3.config;
 
 import java.io.File;
+import java.time.Duration;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -9,7 +10,6 @@ import javax.cache.CacheManager;
 import javax.cache.Caching;
 
 import org.ehcache.config.CacheConfiguration;
-import org.ehcache.config.ResourcePools;
 import org.ehcache.config.builders.CacheConfigurationBuilder;
 import org.ehcache.config.builders.ExpiryPolicyBuilder;
 import org.ehcache.config.builders.ResourcePoolsBuilder;
@@ -17,7 +17,6 @@ import org.ehcache.config.units.EntryUnit;
 import org.ehcache.config.units.MemoryUnit;
 import org.ehcache.core.config.DefaultConfiguration;
 import org.ehcache.impl.config.persistence.DefaultPersistenceConfiguration;
-import org.ehcache.impl.config.store.heap.DefaultSizeOfEngineProviderConfiguration;
 import org.ehcache.jsr107.EhcacheCachingProvider;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.interceptor.KeyGenerator;
@@ -31,54 +30,41 @@ public class CachingConfig {
 
     @Bean
     public JCacheCacheManager jCacheCacheManager(CacheManager cacheManager) {
-        JCacheCacheManager jCacheCacheManager = new JCacheCacheManager();
-        jCacheCacheManager.setCacheManager(cacheManager);
-
-        return jCacheCacheManager;
+        return new JCacheCacheManager(cacheManager);
     }
 
     @Bean(destroyMethod = "close")
     public CacheManager cacheManager(CacheConfiguration<Object, Object> cacheConfiguration) {
         Map<String, CacheConfiguration<?, ?>> caches = new HashMap<>();
 
-        // 여기에캐시 목록 추가
         caches.put("getTest", cacheConfiguration);
 
-        // jsr107CacheManager 공급자
         EhcacheCachingProvider provider = (EhcacheCachingProvider) Caching
                 .getCachingProvider("org.ehcache.jsr107.EhcacheCachingProvider");
 
-        // 물리 disk 방식 사용 시 저장 경로
         String tempFolder = System.getProperty("java.io.tmpdir");
         File tempFile = new File(tempFolder);
 
-        // 캐시 기본 설정
         DefaultConfiguration configuration = new DefaultConfiguration(caches, provider.getDefaultClassLoader(),
-                new DefaultSizeOfEngineProviderConfiguration(100, MemoryUnit.MB, Integer.MAX_VALUE),    // 최대 100MB크기제한, 오브젝트수 무제한 처리
-                new DefaultPersistenceConfiguration(tempFile)); // 영속성 물리 disk 설정
+                new DefaultPersistenceConfiguration(tempFile));
  
         return provider.getCacheManager(provider.getDefaultURI(), configuration);
     }
 
-    // 개별 리소스 풀 설정
     @Bean
-    public ResourcePools resourcePools() {
-        return ResourcePoolsBuilder.newResourcePoolsBuilder()
-                .heap(2000, EntryUnit.ENTRIES)  // 캐시당 2000개 Entry 허용, 사이즈방식 사용 가능
-                // .offheap(200, MemoryUnit.MB)  // 오프힙 사용시 선택 (JVM메모리사용)
-                .disk(1000, MemoryUnit.MB, true) // 디스크 방식 최대 크기 제한
+    public CacheConfiguration<Object, Object> cacheConfiguration() {
+        return CacheConfigurationBuilder.newCacheConfigurationBuilder(Object.class, Object.class, 
+                    ResourcePoolsBuilder.newResourcePoolsBuilder()
+                        .heap(2000, EntryUnit.ENTRIES)  // Heap 계층 pool : 2000개 Entity 저장가능
+                        .disk(1000, MemoryUnit.MB)      // Disk 계층 pool : 디스크는 1g까지
+                        .build())
+                .withExpiry(ExpiryPolicyBuilder.timeToIdleExpiration(Duration.ofSeconds(60))) // 1분간 안쓰면 삭제
+                .withExpiry(ExpiryPolicyBuilder.timeToLiveExpiration(Duration.ofSeconds(300))) // 최초 생성후 5분 유지
+                .withSizeOfMaxObjectSize(100, MemoryUnit.MB)     // 객체 크기는 10MB까지 (기본값 MAX)
+                .withSizeOfMaxObjectGraph(Integer.MAX_VALUE)    // 객체 그래프 복잡성 제한 없음 (기본값 1000)
                 .build();
     }
 
-    // 개별 캐시 설정
-    @Bean
-    public CacheConfiguration<Object, Object> cacheConfiguration(ResourcePools resourcePools) {
-        return CacheConfigurationBuilder.newCacheConfigurationBuilder(Object.class, Object.class, resourcePools)
-                .withExpiry(ExpiryPolicyBuilder.timeToLiveExpiration(java.time.Duration.ofSeconds(300))) // 300초 이후 제거됨
-                .build();
-    }
-
-    // Entry Key 이름을 결정짓는 로직
     @Bean(name = "concatKeyGenerator")
     public KeyGenerator concatKeyGenerator() {
         return (target, method, params) -> {
@@ -96,7 +82,6 @@ public class CachingConfig {
                 if (i != params.length - 1)
                     keyNameBuilder.append(",");
             }
-            System.out.println(keyNameBuilder.toString());
             return keyNameBuilder.toString();
         };
     }
